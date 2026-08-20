39666a244aa3b483d2430e3f5af7142e25589464
jamieandrewss <jamieandrews28.03.06@gmail.com>
2026-08-20 11:45:21 +0100

    added kitty config

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -4,5 +4,8 @@
 
 hl.on("hyprland.start", function()
     hl.exec_cmd("systemctl --user start xdg-desktop-portal-hyprland")
+    hl.exec_cmd("systemctl --user start xdg-desktop-portal-gtk")
     hl.exec_cmd("systemctl --user start hyprpolkitagent")
+
+    hl.exec_cmd("gsettings set org.gnome.desktop.interface cursor-theme 'phinger-cursors-dark'")
 end)
--- hypr/modules/env.lua
+++ hypr/modules/env.lua
@@ -3,4 +3,7 @@
 -------------------
 
 hl.env("XCURSOR_SIZE", "24")
+hl.env("XCURSOR_THEME", "phinger-cursors-dark")
+
 hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_THEME", "phinger-cursors-dark")
