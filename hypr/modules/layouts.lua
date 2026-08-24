60fcd5e7da05650f1cab7f1697614bbc32eb12ce
jamieandrewss <jamieandrews28.03.06@gmail.com>
2026-08-24 11:54:40 +0100

    Added a separate file for layout management in hyprland

---
+++ hypr/modules/layouts.lua
@@ -0,0 +1,15 @@
+---------------
+--- LAYOUTS ---
+---------------
+
+hl.config({
+    general = {
+        layout = "master",
+    },
+
+    master = {
+        new_status = "slave",
+        orientation = "left",
+        mfact = 0.60
+    },
+})
